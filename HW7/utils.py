67b038914d707096c24e6304cdd5b117a11095ba
andrey.isaev <an.isaev7@gmail.com>
2022-08-03 22:10:07 +0300

    add comments

--- HW7/utils.py
+++ HW7/utils.py
@@ -1,11 +1,11 @@
 import os
 import json
+from typing import Union, Any
 
 
 def load_students() -> list:
     """
-    Load student`s list from students.json
-    return: list
+
     """
     student_dir = os.path.join("Data", "students.json")
     with open(student_dir, encoding='utf-8') as file:
@@ -15,8 +15,8 @@ def load_students() -> list:
 
 def load_professions() -> list:
     """
-       Load profession`s list from professions.json
-       return: list
+    Load profession`s list from professions.json
+    :return list
     """
     prof_dir = os.path.join("Data", "professions.json")
     with open(prof_dir, encoding='utf-8') as file:
@@ -24,34 +24,37 @@ def load_professions() -> list:
     return professions_data
 
 
-def get_student_by_pk(pk: int) -> dict:
+def get_student_by_pk(pk: int) -> Union[bool, Any]:
     """
     Get dict with student data by his pk
     """
-    student_dict = {}
     for student in load_students():
         if student["pk"] == pk:
-            student_dict[student["full_name"]] = student["skills"]
-    return student_dict
+            return student
+    return False
 
 
-def get_profession_by_title(title: str) -> dict:
+def get_profession_by_title(title: str) -> Union[bool, Any]:
     """
-        Get dict with profession data by title
+    Get dict with profession data by title
     """
-    prof_dict = {}
     for profession in load_professions():
-        if profession["title"] == title:
-            prof_dict[profession["title"]] = profession["skills"]
-    return prof_dict
+        if profession["title"].lower() == title.lower():
+            return profession
+    return False
 
 
-def check_fitness(student, profession) -> dict:
+def check_fitness(student: dict, profession: dict) -> dict:
     """
-
+    Get student dict and profession dict return dictionary example:
+    {
+      "has": ["Python", "Linux"],
+      "lacks": ["Docker, SQL"],
+      "fit_percent": 50
+    }
     """
-    user_set = set(list(student.values())[0])
-    prof_set = set(list(profession.values())[0])
+    user_set = set(student["skills"])
+    prof_set = set(profession["skills"])
     fit_percent = len(user_set.intersection(prof_set)) / len(prof_set) * 100
     return {"has": list(user_set.intersection(prof_set)),
             "lacks": list(set(prof_set).difference(set(user_set))),
@@ -59,7 +62,10 @@ def check_fitness(student, profession) -> dict:
             }
 
 
-def print_fitness(student, fitness_dict):
+def print_fitness(student: str, fitness_dict: dict) -> None:
+    """
+    Print fitness
+    """
     print(f"Пригодность {int(fitness_dict['fit_percent'])}%")
     print(f"{student} знает {', '.join(fitness_dict['has'])}")
     print(f"{student} не знает {', '.join(fitness_dict['lacks'])}")
